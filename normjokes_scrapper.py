c5d7f106beb2a85d5486be76f7df5f8b480bfaf9
lukeo <lukeo@automationit.com>
2021-11-05 10:17:18 +0800

    Updated to return joke namedtuple

--- normjokes_scrapper.py
+++ normjokes_scrapper.py
@@ -1,14 +1,13 @@
 from requests_html import HTMLSession
+from collections import namedtuple
 
 def get_joke(number):
     session = HTMLSession()
     r = session.get('https://normjokes.com/' + str(number))
     r.html.render()
-    joke = r.html.find('#joke', first=True)
+    joketext = r.html.find('#joke', first=True)
     ep = r.html.find('#ep', first=True)
     guest = r.html.find('#guest', first=True)
-    print("Joke:")
-    print(joke.text.replace('\n','\n\n'))
-    print("\nEpisode:")
-    print(ep.text)
-    print(guest.text)
+    Joke = namedtuple("Joke", "text ep guest")
+    out = Joke(joketext.text, ep.text, guest.text)
+    return out
