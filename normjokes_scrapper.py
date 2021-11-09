97344afdf9fb0755c5289bb95cb5b0b5d8c95106
lukeo <lukeo@automationit.com>
2021-11-09 10:41:26 +0800

    Added get_jokelist function

--- normjokes_scrapper.py
+++ normjokes_scrapper.py
@@ -1,13 +1,26 @@
 from requests_html import HTMLSession
 from collections import namedtuple
 
+session = HTMLSession()
+Joke = namedtuple("Joke", "text ep guest")
+
 def get_joke(number):
-    session = HTMLSession()
     r = session.get('https://normjokes.com/' + str(number))
     r.html.render()
     joketext = r.html.find('#joke', first=True)
     ep = r.html.find('#ep', first=True)
     guest = r.html.find('#guest', first=True)
-    Joke = namedtuple("Joke", "text ep guest")
     out = Joke(joketext.text, ep.text, guest.text)
     return out
+
+def get_jokelist():
+    jokelist = []
+    print('Loaded 000 of 407 jokes')
+    for i in range(407):
+        jokelist.append(get_joke(i))
+        print('Loaded ' + f'{(i+1):03}' + ' of 407 jokes')
+    else:
+        print('')
+        print('Jokes loaded')
+    
+    return jokelist
